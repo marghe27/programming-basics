3004d22bbe5f7d0836d14c0fd932eec58dbc9900
marghe27 <marghe27@live.it>
2022-06-18 12:00:09 +0200

    solution for exercise display-the-head-of-a-file

---
+++ projects/m4/001-display-the-head-of-a-file/solutions/margheritabonanno/README.md
@@ -0,0 +1,15 @@
+# PROJECT NAME
+Display the Head of a File
+
+
+# Instructions
+Write a Javascript program that displays the first 10 lines of a file whose name is provided as a command line argument. Display an appropriate error message if the file requested by the user does not exist, or if the command line argument is omitted.
+
+# Documentation
+
+Some useful challenge links:
+https://www.html5rocks.com/en/tutorials/file/dndfiles//#toc-reading-files
+
+# Deadline
+
+An estimate of the time required to implement the solution
--- projects/m4/001-display-the-head-of-a-file/solutions/margheritabonanno/solution.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/margheritabonanno/solution.js
@@ -1,1 +1,63 @@
 // display-the-head-of-a-file 
+
+"use strict";
+
+try {
+
+    function readText(fileName){
+
+        // create element input with type file
+        let element = document.createElement('input');
+        element.setAttribute('type',"file");
+        element.setAttribute('id', "btnOpenFile");
+        element.onchange = function(){
+            readText(this);
+        }
+
+        //create style to display input element
+        element.style.display = 'block';
+        element.style.marginLeft = '10px';
+        document.body.appendChild(element);
+
+        //a table to display the result
+        let table = document.querySelector("table");
+        table.before(element); 
+    
+        //search file
+        element.click();
+
+        let reader;
+        // If FileReader is supported
+        if (window.File && window.FileReader){
+            reader = new FileReader();
+
+        } else{
+            alert('FileReader not supported in your browser');
+            return false;
+        }
+        // If the file to read is the one passed as argument: 
+        let argument = (fileName.file[0])? true : false;
+
+        if(argument){
+            console.log(fileName.file[0].name);
+            reader.onload = function(event) {
+            let output = event.target.result;
+            parseResult(output);
+            };
+        reader.readAsText(fileName.file[0]);
+        } else {
+            errorhandler();
+            return false;
+        }
+        return true;
+    }
+let errorhandler = () => alert("This is not the requested file");
+
+function parseResult(output){
+    //
+}
+    
+} catch (error) {
+    document.getElementById("errorMessage").innerHTML = error.message;
+    console.log(error.message);
+}
