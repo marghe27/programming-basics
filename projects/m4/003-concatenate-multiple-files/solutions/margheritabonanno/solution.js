376041582164c3a2b20ceeb0cfac0e720427427f
marghe27 <marghe27@live.it>
2022-07-15 20:20:19 +0200

    Use of fetch to get and read files

--- projects/m4/003-concatenate-multiple-files/solutions/margheritabonanno/solution.js
+++ projects/m4/003-concatenate-multiple-files/solutions/margheritabonanno/solution.js
@@ -2,51 +2,68 @@
 
 "use strict";
 
-//Use PROMISES
-// Promise to create input elements
+//PROMISES
+// use of FETCH to get and read elements
 
-const creaInputElement = new Promise ((resolve, reject)=>{
-        return resolve();         
-          
-});
 
-creaInputElement.then(()=>{
-    // function to open file with input element
-     openFile();  
-     readFile(this);        
+const functionFetch = (src)=>{
+
+      fetch(src)
+
+      .then((response)=>{
+         console.log(response); 
+         return response.text()
+       })
             
-});
+      .then((data)=>{
+         console.log("Data "+data);
+         
+      })
 
-function openFile(){
-            // DOM manipulation
-            let element = document.createElement('input');
-            element.setAttribute('type',"file"); 
-            element.setAttribute('id', "btnOpenFile");
-            element.setAttribute('multiple','');
-
-           //it doesn't work
-            element.onChange = () => {
-                //it doesn't work
-               alert("Funziona On Change");
-               
-            }; 
-            //create style to display input element
-            element.style.display = 'block';
-            element.style.marginLeft = '10px';
-            document.body.appendChild(element);
-            //search file
-            element.click();
-
+      .catch((error)=>{
+         console.log("Errore "+error);
+      })
 };
 
-// Function to read files open with input
-function readFile(inputObj) {
-    
-    /*  @param inputObj is OBJECT? */
-    Object.entries(inputObj).map(([key, value]) => console.log(`${key}: ${value}`));
-    
-    
+
+// function to create array of files
+const getFiles = ()=>{
+   const arrFiles = [];
+   const text_00 = 'http://localhost:8888/margheritabonanno/acronimi_divertenti.txt';
+   const text_01 = 'http://localhost:8888/margheritabonanno/fileProva.txt';
+   
+   arrFiles.push(text_00);
+   arrFiles.push(text_01);
+   
+   let text = "";
+
+   for (let i of arrFiles) {
+         text += i; 
+         console.log(`file: ${text}`);
+      
+   }
+      return arrFiles;
+  
 }
+// assignment of return value of getFiles() to returnValue
+ const returnValue = getFiles();
+   console.log(" get file "+returnValue[1]+" \n\n\r "+returnValue[0]);
+
+ // array di Promises 
+ const promiseList = []; 
+ promiseList.push(functionFetch(returnValue[0]));
+ promiseList.push(functionFetch(returnValue[1]));
+   console.log(" prova promiseList " + promiseList );
+ 
+ 
+// Promise All takes  the array of strings
+ Promise.all(promiseList)
+      
+      .then((data)=>{
+         console.log("Promise All "+data[0]+" \n\n\r "+data[1]);
+         return data;
+      });
+
     
          
 
