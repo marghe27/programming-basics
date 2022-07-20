a09ce903f2fddd00c663a6665b3ed2ef62d038c1
marghe27 <marghe27@live.it>
2022-07-20 12:26:06 +0200

    Bug fix

--- projects/m4/003-concatenate-multiple-files/solutions/margheritabonanno/solution.js
+++ projects/m4/003-concatenate-multiple-files/solutions/margheritabonanno/solution.js
@@ -8,21 +8,16 @@
 
 const functionFetch = (src)=>{
 
-      fetch(src)
+   return fetch(src)
 
       .then((response)=>{
-         console.log(response); 
-         return response.text()
+         return response.text();
        })
-            
-      .then((data)=>{
-         console.log("Data "+data);
-         
-      })
 
       .catch((error)=>{
          console.log("Errore "+error);
-      })
+      }) 
+      
 };
 
 
@@ -45,24 +40,38 @@ const getFiles = ()=>{
       return arrFiles;
   
 }
-// assignment of return value of getFiles() to returnValue
- const returnValue = getFiles();
-   console.log(" get file "+returnValue[1]+" \n\n\r "+returnValue[0]);
+// assignment of return value of getFiles() to arr
+const arr = getFiles();
+   console.log(" get file "+arr[1]+" \n\n\r "+arr[0]);
 
- // array di Promises 
- const promiseList = []; 
- promiseList.push(functionFetch(returnValue[0]));
- promiseList.push(functionFetch(returnValue[1]));
-   console.log(" prova promiseList " + promiseList );
- 
- 
-// Promise All takes  the array of strings
- Promise.all(promiseList)
-      
-      .then((data)=>{
-         console.log("Promise All "+data[0]+" \n\n\r "+data[1]);
-         return data;
-      });
+// function to create array of Promise
+const getPromises = () => {
+   const promiseArray = []; 
+   promiseArray.push(functionFetch(arr[0]));
+   promiseArray.push(functionFetch(arr[1]));
+   console.log("promiseArray "+ promiseArray);
+   return promiseArray;
+}
+
+// assignment of return value of getPromises() to getPromisesArray
+const getPromisesArray = getPromises(); 
+console.log(" getPromises() "+ getPromisesArray);
+
+// Promise.all to read, with response.text(), ALL Promise fetched 
+const promiseAll = Promise.all(getPromisesArray)
+                  .then(values => {
+                     
+                     for (const value of values) {
+                        console.log(" value "+value);
+                     } 
+                     return values;
+                  })
+                  
+                  .catch(error => {
+                     console.error(error.message)
+                  });
+
+console.log("promiseAll "+ promiseAll); 
 
     
          
